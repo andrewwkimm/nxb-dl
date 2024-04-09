08d6abab3693ecae7540034b240abaf4793becb9
Andrew Kim <andrewkimka@gmail.com>
2024-04-09 01:22:21 -0400

    Starts CLI for nxb-dl

--- src/nxbdl/cli.py
+++ src/nxbdl/cli.py
@@ -1,1 +1,32 @@
-"""CLI for nxb-dl."""
+"""The nxb-dl CLI."""
+from typing import Optional
+
+import click
+
+from nxbdl.api import nxbdlAPI
+from nxbdl.exceptions import NoViableLinkError
+
+
+@click.command()
+@click.argument("urls", nargs=-1, required=True)
+@click.option("--resolution", "-r", default=None, help="Resolution of the video")
+def main(urls: str, resolution: Optional[str]):
+    """Download video(s) using nxb-dl."""
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    RESET = "\033[0m"
+
+    nxbdl = nxbdlAPI()
+
+    try:
+        click.echo(GREEN + "Downloading video(s)..." + RESET)
+
+        nxbdl.download(urls, resolution)
+
+        click.echo(GREEN + "Download(s) successful!" + RESET)
+    except NoViableLinkError as excinfo:
+        click.echo(f"{RED}NoViableLinkError: {excinfo}{RESET}")
+
+
+if __name__ == "__main__":
+    main()
