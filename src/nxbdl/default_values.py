c345a9976eb4ef3bdabaa3fae386cfccfaeb0ce7
Andrew Kim <andrewkimka@gmail.com>
2024-04-10 15:19:17 -0400

    Add default values for the drivers

---
+++ src/nxbdl/default_values.py
@@ -0,0 +1,20 @@
+"""Default values for nxb-dl."""
+
+from pathlib import Path
+from typing import List
+
+
+class DefaultValues:
+    """Default values."""
+
+    resolutions: List[str] = ["1440", "1080", "720", "520"]
+    download_path: Path = Path("~/Downloads")
+
+
+class ninexbuddy:
+    """Default values for 9xbuddy.com."""
+
+    downloads_xpath: str = "/html/body/main/section/section[3]/section[2]"
+    tag_element = "a"
+    url: str = "https://9xbuddy.in/process?url="
+    video_name_xpath: str = "/html/body/main/section/section[3]/div[2]"
