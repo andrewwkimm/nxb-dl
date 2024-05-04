3cf3757bd5cdd09b149eff141ecce00dbfc80d27
Andrew Kim <andrewkimka@gmail.com>
2024-05-04 14:39:32 +0000

    Import version number dynamically

--- tests/test_nxbdl.py
+++ tests/test_nxbdl.py
@@ -1,8 +1,10 @@
 """Tests for nxbdl."""
 
+from importlib.metadata import version
+
 from nxbdl import __version__
 
 
 def test_version() -> None:
     """Tests that version is set to expected value."""
-    assert __version__ == "0.1.0"
+    assert __version__ == version("nxbdl")
