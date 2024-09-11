f4f64f07b3d061f97f9a42a08142f982d72dda49
Andrew Kim <andrewkimka@gmail.com>
2024-09-11 09:07:30 +0000

    Correct workspace path for safe directory configuration

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configure baseline git settings
-git config --global --add safe.directory ${containerWorkspaceFolder}
+git config --global --add safe.directory $(pwd)
 git config --global user.email "andrewkimka@gmail.com"
 git config --global user.name "Andrew Kim"
 
