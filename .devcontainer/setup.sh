8bafa8aa87dcb96619c5e3bc8d97242732666cfe
Andrew Kim <andrewkimka@gmail.com>
2024-09-11 09:13:09 +0000

    Add prompt for username and email at the start

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Configure baseline git settings
+# Add current workspace as a safe directory
 git config --global --add safe.directory $(pwd)
-git config --global user.email "andrewkimka@gmail.com"
-git config --global user.name "Andrew Kim"
+
+# Prompt user for Git username and email
+read -p "Enter your Git username: " git_username
+read -p "Enter your Git email: " git_email
+
+# Configure baseline git settings
+git config --global user.email "$git_email"
+git config --global user.name "$git_username"
 
 # Setup environment
 make setup
